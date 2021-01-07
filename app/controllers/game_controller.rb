6751c6825dddaf73c0a098d214b17f627ca9424a
SSH002 <you@example.com>
2021-01-07 19:27:59 +0200

    Deleted ErrorsController

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,7 +7,7 @@ class GameController < ApplicationController
       return
     end
 
-    @account = get_account
+    @account = account_get
     check_end_game
     @warning = session[:last_warn] || ''
   end
@@ -16,12 +16,12 @@ class GameController < ApplicationController
     render plain: 'Access error', status: :unauthorized unless session[:user_id].nil?
   end
 
-  def get_account
+  def account_get
     Account.find_by(id: session[:user_id])
   end
 
   def check_end_game
-    result = get_account.check_win_lose
+    result = account_get.check_win_lose
 
     if result == ''
       @is_lose = false
@@ -32,39 +32,39 @@ class GameController < ApplicationController
   end
 
   def start_new_game
-    get_account.init_stats
+    account_get.init_stats
 
     session[:last_warn] = ''
     redirect_to '/game'
   end
 
   def go_job
-    session[:last_warn] = get_account.go_job
+    session[:last_warn] = account_get.go_job
     redirect_to '/game'
   end
 
   def rest
-    session[:last_warn] = get_account.rest
+    session[:last_warn] = account_get.rest
     redirect_to '/game'
   end
 
   def watch_serial
-    session[:last_warn] = get_account.watch_serial
+    session[:last_warn] = account_get.watch_serial
     redirect_to '/game'
   end
 
   def drink_with_marginals
-    session[:last_warn] = get_account.drink_with_marginals
+    session[:last_warn] = account_get.drink_with_marginals
     redirect_to '/game'
   end
 
   def sleep
-    session[:last_warn] = get_account.sleep
+    session[:last_warn] = account_get.sleep
     redirect_to '/game'
   end
 
   def sing
-    session[:last_warn] = get_account.sing
+    session[:last_warn] = account_get.sing
     redirect_to '/game'
   end
 end
